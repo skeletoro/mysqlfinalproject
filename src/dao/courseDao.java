f0b5fc3d8366ca7a1642f3ba4756c6cd0cdfeb57
denisedayz <69566779+denisedayz@users.noreply.github.com>
2020-11-02 16:27:17 -0700

    trying again to show coursedao

--- src/dao/courseDao.java
+++ src/dao/courseDao.java
@@ -1,5 +1,20 @@
 package dao;
 
-public class courseDao {
+import java.sql.ResultSet;
 
+public class courseDao {
+	
+	private final String GET_COURSES_QUERY = "SELECT c.course_id, c.course_name, c.format, c.semester, i.instructor_name FROM courses c, instructors i WHERE c.instructor_id = i.instructor_id";
+
+	
+	
+	public List <Courses> getCourses() {
+		ResultSet rs = connection.prepareStatement(GET_COURSES_QUERY).executeQuery();
+		List <Courses> courses = newArrayList <Courses>();
+		while rs.next()){
+			//I don't completely understand what Nick did here...but the idea is the results here get fed into display courses method in menu
+		}
+		return courses;
+		}
+	}
 }
