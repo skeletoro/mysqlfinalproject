000b2f738c0639d8dadd3900d95b4663b49f0cad
skeletoro <69654563+skeletoro@users.noreply.github.com>
2020-11-02 11:30:24 -0700

    small upload

--- src/application/menu.java
+++ src/application/menu.java
@@ -12,6 +12,64 @@ public class menu {
 			showMenu();
 			selection = scanner.nextLine();
 			
+			if (selection.equals("1")) {
+				
+				displayCourses();
+				
+			}else if (selection.equals("2")) {
+				
+				viewCourse();
+				
+			}else if (selection.equals("3")) {
+				
+				viewStudent();
+				
+			}else if (selection.equals("4")) {
+				
+				viewInstructor();
+				
+			}else if (selection.equals("5")) {
+				
+				addCourse();
+				
+			}else if (selection.equals("6")) {
+				
+				addInstructor();
+				
+			}else if (selection.equals("7")) {
+				
+				addStudent();
+				
+			}else if (selection.equals("8")) {
+				
+				deleteCourse();
+				
+			}else if (selection.equals("9")) {
+				
+				deleteInstructor();
+				
+			}else if (selection.equals("10")) {
+				
+				deleteStudent();
+				
+			}else if (selection.equals("11")) {
+				
+				updateCourse();
+				
+			}else if (selection.equals("12")) {
+				
+				updateStudent();
+				
+			}else if (selection.equals("13")) {
+				
+				updateInstructor();
+				
+			}else {
+				System.out.println("please make a valid selection");
+			}
+			
+			System.out.println("Press enter to continue");
+			scan.nextLine();
 			
 		}while (!selection.equals("14"))
 	}
