637194bfa321cfb4d44af15992dffa4874696d0a
denisedayz <69566779+denisedayz@users.noreply.github.com>
2020-11-03 11:53:03 -0700

    started methods in instructorDao

--- src/dao/instructorDao.java
+++ src/dao/instructorDao.java
@@ -1,6 +1,9 @@
 package dao;
 
+import java.sql.PreparedStatement;
+
 public class instructorDao {
+
 	
 	private final String ADD_NEW_INSTRUCTOR_QUERY = "INSERT INTO instructors (instructor_name, instructor_email) VALUES (?,?)";
 	
@@ -8,5 +11,30 @@ public class instructorDao {
 	
 	private final String UPDATE_INSTRUCTOR_BY_ID_QUERY = "UPDATE instructors SET instructor_name = ?, instructor_email = ? WHERE instructor_id = ?";
 
+	public instructorDao() {
+		connection = DBconnection.getConnection();
+	}
 	
+	public void addInstructor(String instructorName, String instructorEmail) {
+		PreparedStatement ps = connection.prepareStatement (ADD_NEW_INSTRUCTOR_QUERY);
+		ps.setString(1, instructorName);
+		ps.setString(2, instructorEmail);
+		ps.executeUpdate();
+	}
+
+	public void deleteInstructor(int instructorId) {
+		PreparedStatement ps = connection.prepareStatement (DELETE_INSTRUCTOR_BY_ID_QUERY);
+		ps.setInt(1, instructorId);
+		ps.executeUpdate();
+	}
+	
+	public void updateInstructor(String instructorName, String instructorEmail, int instructorId) {
+		PreparedStatement ps = connection.prepareStatement (UPDATE_INSTRUCTOR_BY_ID_QUERY);
+				ps.setString(1, instructorName);
+				ps.setString(2, instructorEmail);
+				ps.setInt(3, instructorId);
+				ps.executeUpdate();				
+		
+	}
+
 }
