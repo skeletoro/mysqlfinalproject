c8162d9f3df3bea9244089eea7246f21376bc384
unknown <anissaeliette@gmail.com>
2020-11-03 11:37:16 -0800

    Database Connection

---
+++ DbConnection.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+	private static final String url = "jdbc:mysql://localhost:3306/bootCamp";
+	private static final String username = "root";
+	private static final String password = "1230";
+	private static Connection conn;
+	private static DbConnection instance;
+	
+	
+	
+	private DbConnection (Connection conn) {
+		this.conn = conn;
+
+	}
+	
+	public static Connection getConnection() {
+		
+		if (instance == null) {
+			
+		try {
+            conn = DriverManager.getConnection(url, "username", "password");
+            instance = new DbConnection(conn);
+			
+			System.out.println("Succefully connected");
+			
+			
+		} catch (SQLException e) {
+			System.out.println("Unable to connect, try again");
+			e.printStackTrace();
+		}
+		return DbConnection.conn;
+		
+	}
+		return conn;
+	}
+}
+
+
