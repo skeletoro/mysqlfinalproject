0559a78dff02597269d9c9917bf75ad2a5217b35
skeletoro <69654563+skeletoro@users.noreply.github.com>
2020-11-06 18:01:09 -0700

    working menu upload

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -5,15 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-<<<<<<< HEAD
-	private static final String url = "jdbc:mysql://localhost:3306/bootCamp";
-	private static final String username = "root";
-	private static final String password = "Zakkary123";
-=======
+
 	private final static String url = "jdbc:mysql://localhost:3306/bootcamp";
 	private final static String username = "root";
-	private final static String password = "password";
->>>>>>> befe4d1fc5c723a3c5d534285ca9e80a6fee4c1f
+	private final static String password = "YOUR PW HERE !!!! leave this blank when you push please";
 	private static Connection connection;
 	private static DBConnection instance;
 
@@ -25,61 +20,29 @@ public class DBConnection {
 	public static Connection getConnection() {
 
 		if (instance == null) {
-<<<<<<< HEAD
-			
+
+		}
 		try {
-            connection= DriverManager.getConnection(url, "root", "Zakkary123");
-            instance = new DBConnection(connection);
-			
-			System.out.println("Succefully connected");
-			
-			
+			connection = DriverManager.getConnection(url, username, password);
+			instance = new DBConnection(connection);
+
+			System.out.println("Successfully connected");
+
 		} catch (SQLException e) {
 			System.out.println("Unable to connect, try again");
 			e.printStackTrace();
-		}
-		/*finally {
-			if (instance != null) {
-				System.out.println("______________________\nclosing connection now");
-				try {
-					connection.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
+		} return DBConnection.connection;
 				}
-				System.out.println("Connection closed successfully"); // for closing connection very important to 
-			}*/
+				/*
+				 * finally { if (instance != null) {
+				 * System.out.println("______________________\nclosing connection now"); try {
+				 * connection.close(); } catch (SQLException e) { e.printStackTrace(); }
+				 * System.out.println("Connection closed successfully"); // for closing
+				 * connection very important to }
+				 */
 
 		
-	
-	}
-return DBConnection.connection;
-	}		
-=======
-
-			try {
-				connection = DriverManager.getConnection(url, username, password);
-				instance = new DBConnection(connection);
-
-				System.out.println("Successfully connected");
-
-			} catch (SQLException e) {
-				System.out.println("Unable to connect, try again");
-				e.printStackTrace();
-			} finally {
-				if (instance != null) {
-					System.out.println("______________________\nclosing connection now");
-					try {
-						connection.close();
-						System.out.println("Connection closed successfully"); // for closing connection very important to
-
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-
-			}
 		}
-		return DBConnection.connection;
->>>>>>> befe4d1fc5c723a3c5d534285ca9e80a6fee4c1f
-	}
-}
+		
+
+	
